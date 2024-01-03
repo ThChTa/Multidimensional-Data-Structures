1b9c4f0f3a8bd12b00e0b8a8e64df7f8d7fcf452
Theodosios Chronopoulos <theodosis14@yahoo.gr>
2024-01-03 23:25:36 +0200

    comments

--- trees/r-tree.py
+++ trees/r-tree.py
@@ -2,45 +2,45 @@ import pandas as pd
 from rtree import index
 
 
-
+#Μετατροπή γράμματος σε αριθμό 
 def letter_to_number(letter):
     return ord(letter.upper()) - 65
 
 class RTreeIndexer:
     def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
-        self.datas = []
+        self.data = pd.read_csv(csv_file)#διαβάζουμε το csv αρχείο
+        self.datas = []#εδώ θα γίνει αποθήκευση των δεδομένων από το csv αρχείο για χρήση στα queries
+        #εδώ φτιάχνουμε τα properties του r-tree για 3 διαστάσεις
         p = index.Property()
-        p.dimension = 3
+        p.dimension = 3 
         p.dat_extension = 'data'
         p.idx_extension = 'index'
-        self.idx3d = index.Index(properties=p)
+        self.idx3d = index.Index(properties=p) 
+        #φτιάχνουμε το index με τα δεδομένα
         self._build_index()
 
     def _build_index(self):
+        #insert στο r-tree
         for i, row in self.data.iterrows():
-            # Αντικαταστήστε 'surname', 'awards', 'DBLP_Record' με τα σωστά ονόματα στηλών
+            
             surname = letter_to_number(row['lastName'][0])
             awards = row['awards']
             dblp_record = row['dblp_records']
             education = row['education']
-            
-
-            # Αντικαταστήστε αυτές τις συντεταγμένες με τη σωστή λογική μετατροπής στοιχείων σε συντεταγμένες
-            self.idx3d.insert(i, (surname, awards, dblp_record, surname, awards, dblp_record))
-            data =(row['lastName'], row['awards'], row['education'], row['dblp_records'])
+            self.idx3d.insert(i, (surname, awards, dblp_record, surname, awards, dblp_record)) #πέρασμα row στο r-tree
+            data =(row['lastName'], row['awards'], row['education'], row['dblp_records'])#αποθήκευση στην λίστα μας
             self.datas.append(data)
-            # print(self.idx3d)
             
 
     def query(self, left_letter, right_letter, awards_min, dblp_min, dblp_max):
-        # Αντικαταστήστε αυτές τις συντεταγμένες με τη σωστή λογική μετατροπής των ερωτημάτων σε συντεταγμένες
+        
         left_letter=letter_to_number(left_letter)
         right_letter=letter_to_number(right_letter)
-        query_coordinates = (float(left_letter), awards_min, dblp_min, float(right_letter), float('inf'), dblp_max)
-        # print([i for i in self.idx3d.intersection(query_coordinates)])
+        query_coordinates = (float(left_letter), awards_min, dblp_min, float(right_letter), float('inf'), dblp_max)#συντεταγμένες του query για να πάρουμε τα σχετικά αποτελέσματα
+        
         results = list(self.idx3d.intersection(query_coordinates))
         print(results)
+        #δομημένη επιστροφή των αποτελεσμάτων της αναζήτησης για χρήση τους στην πορεία
         query_results = []
         for result in results:
             lastName, awards, education, dblp_records = self.datas[result]
